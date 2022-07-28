3b969df2a28f6b0c515adf2492a839d70fa3c2f5
Donovan Knight <99293948+dkthedeveloper@users.noreply.github.com>
2022-07-28 09:09:04 -0700

    added code that sets default user location to New York City if geolocation is not allowed for some reason

--- JS/location.js
+++ JS/location.js
@@ -1,11 +1,14 @@
-let userLatitude
-let userLongitude
+let userLatitude;
+let userLongitude;
+const defaultLattitude = 40.7648;   // New York City coordinates
+const defaultLongitude = -73.9808;
 
 
 // https://www.javascripttutorial.net/web-apis/javascript-geolocation/
 function getUserLocation () {
     if (!navigator.geolocation) {
         console.error(`Your browser doesn't support Geolocation`);
+        onError()
     }  else {
        console.log('your browser supports Geo!')
        navigator.geolocation.getCurrentPosition(onSuccess, onError)
@@ -24,6 +27,8 @@ function onSuccess(position) {
 // handle error case
 function onError() {
     console.log(`Failed to get your location!`);
+    userLatitude = defaultLattitude;
+    userLongitude = defaultLongitude;
 };
 
 export { userLatitude, userLongitude, getUserLocation }
